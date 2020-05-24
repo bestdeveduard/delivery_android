02539f5ccd64f36ea26efea7dacb336eaf233d83
Tomas <tomasmalix518@gmail.com>
2020-05-24 05:16:37 -0500

    android app
    delivery app

---
+++ sasaride_user/app/src/main/java/com/sasaride/user/utils/Url.java
@@ -0,0 +1,37 @@
+package com.sasaride.user.utils;
+
+/**
+ * Created by techintegrity on 04/07/16.
+ */
+public class Url {
+
+    public static String baseUrl = "https://sasaride.co.ke/app/web_service/";
+    //public static String baseUrl = "http://v1technology.co.uk/demo/naqil/naqilcom/Source/web_service/";
+    public static String signupUrl =baseUrl+"sign_up";
+    //public static String signupUrl = "https://sasaride.co.ke/app/test.php";
+    public static String loginUrl =baseUrl+"login";
+    public static String forgotPasswordUrl =baseUrl+"forgot_password";
+    public static String facebookLoginUrl =baseUrl+"facebook_login";
+    public static String twitterLoginUrl =baseUrl+"twitter_login";
+    public static String bookCabUrl = baseUrl+"book_cab";
+    public static String loadTripsUrl = baseUrl+"load_trips";
+    public static String loadTripsFiltersUrl = baseUrl+"filter_book";
+    public static String profileUrl = baseUrl+"profile_edit";
+    public static String changePasswordUrl = baseUrl+"change_password";
+    public static String deleteCabUrl = baseUrl+"user_reject_trip";
+    public static String FixAreaUrl = baseUrl+"fix_area_list";
+
+    public static String userImageUrl = "https://sasaride.co.ke/app/user_image/";
+    public static String carImageUrl = "https://sasaride.co.ke/app/car_image/";
+    public static String DriverImageUrl = "https://sasaride.co.ke/app/driverimages/";
+
+    public static String socketUrl = "http://162.243.225.225:4040";
+
+    public static String subscribeUrl = "http://162.243.225.225:8001/subscribe";
+    public static String unsubscribeUrl = "http://162.243.225.225:8001/unsubscribe";
+
+    public static String FacebookImgUrl = "https://graph.facebook.com/";
+
+    public static String SocialShareUrl = "https://sasaride.co.ke/app/web_service/share-page";
+    public static String AppLogUrl = "https://sasaride.co.ke/app/application/views/img/app-logo-new.png";
+}
